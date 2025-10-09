0d6d6876739f592bc5999ca99609d27ae9797feb
mahesh-veesam <veesammaheshmahee@gmail.com>
2025-10-09 20:50:02 +0530

    changed files for deployment ready

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:  "https://vpath-testing.onrender.com/" ,
+  baseURL:  "https://vpath.onrender.com/" ,
   headers: {
     'Content-Type': 'multipart/form-data',
   },
