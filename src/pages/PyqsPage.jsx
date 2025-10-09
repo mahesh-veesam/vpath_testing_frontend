3bf94c2f590e207743aefbd08f7e169d7090bf05
mahesh-veesam <veesammaheshmahee@gmail.com>
2025-10-09 18:58:20 +0530

    changed files for deployment ready

--- src/pages/PyqsPage.jsx
+++ src/pages/PyqsPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Select from '@/components/SelectPyq'
 import {Flex} from '@chakra-ui/react'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams, useNavigate , Outlet } from 'react-router-dom'
 
 const PyqsPage = () => {
   const { batch } = useParams();
@@ -10,6 +10,7 @@ const PyqsPage = () => {
     <div>
       <Flex justify="center" fontWeight="600" fontSize={20} mt={1}>Question Papers</Flex>
       <Select batch={batch}/>
+      <Outlet/>
     </div>  
   )
 }
